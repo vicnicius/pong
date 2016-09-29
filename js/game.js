5aa00eaff9318a451b8d98c744979b748b79a37b
Vinicius Andrade <vini.andrade.dev@gmail.com>
2016-09-29 09:11:41 -0300

    Adds animation and collision detection

--- js/game.js
+++ js/game.js
@@ -1,23 +1,58 @@
 function Game () {
   const eng = Engine(document.getElementById('game').getContext('2d'))
   const state = {
-    player1: 25,
+    player1y: 25,
+    player1x: 775,
     score1: 0,
-    player2: 25,
+    player2y: 25,
+    player2x: 0,
     score2: 0,
-    ball: [65, 65]
+    ball: [65, 65],
+    speed: [9, 0] //Speed, Angle (rad)
+  };
+  const ballDx = () => state.ball[0] + state.speed[0] * Math.cos(state.speed[1])
+  const ballDy = () => state.ball[1] + state.speed[0] * Math.sin(state.speed[1])
+  const newSpeed = () => {
+    const y = state.ball[1]
+    const x = state.ball[0]
+    let resultSpeed = state.speed[0]
+    let resultAngle = state.speed[1]
+
+    // If the ball colides with the game top-bottom screen limit
+    if (y >= 800 || y <= 0) {
+      resultAngle = state.speed[1] - Math.PI
+    }
+
+    // If the ball colides with one of the players
+    if (x + 15 >= state.player1x || x  <= state.player2x + 25) {
+      resultSpeed = state.speed[0] * -1
+      drawPlayers()
+    }
+
+    return [resultSpeed, resultAngle]
   }
-  eng.drawPlayer(775, state.player1)
-  eng.drawPlayer(0, state.player2)
+  let cache = {
+    player1: [0, 0],
+    player2: [0, 0]
+  };
+
   eng.drawScores(state.score1, state.score2)
   eng.drawNet()
 
+  function drawPlayers () {
+      eng.drawPlayer(state.player1x, state.player1y)
+      eng.drawPlayer(state.player2x, state.player2y)
+  }
+
   function draw () {
-    state.ball = [state.ball[0] + 8, state.ball[1]]
+    state.ball = [ballDx(), ballDy()]
+    state.speed = newSpeed()
     eng.drawBall(state.ball)
+    drawPlayers()
     window.requestAnimationFrame(draw)
   }
   // window.setInterval(draw, 1000 / 60)
+
   window.requestAnimationFrame(draw)
 }
 
