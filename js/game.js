5dd3719ba574cc2a324f76f7fd9ee996f0d0100f
Vinicius Andrade <vini.andrade.dev@gmail.com>
2016-10-01 12:27:42 -0300

    Adds player inputs logic

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 function Game () {
-  const eng = Engine(document.getElementById('game').getContext('2d'))
+  const gameEl = document.getElementById('game')
+  const eng = Engine(gameEl.getContext('2d'))
   const state = {
     player1y: 25,
     player1x: 775,
@@ -32,12 +33,12 @@ function Game () {
     return [resultSpeed, resultAngle]
   }
 
-  eng.drawScores(state.score1, state.score2)
-  eng.drawNet()
-
+  let player1Last, player2Last
   function drawPlayers () {
-    eng.drawPlayer(state.player1x, state.player1y)
-    eng.drawPlayer(state.player2x, state.player2y)
+    eng.drawPlayer(state.player1x, state.player1y, player1Last)
+    eng.drawPlayer(state.player2x, state.player2y, player2Last)
+    player1Last = [state.player1x, state.player1y, 25, 100]
+    player2Last = [state.player2x, state.player2y, 25, 100]
   }
 
   function draw () {
@@ -45,11 +46,12 @@ function Game () {
     state.speed = newSpeed()
     eng.drawBall(state.ball)
     drawPlayers()
+    eng.drawScores(state.score1, state.score2)
+    eng.drawNet()
     window.requestAnimationFrame(draw)
   }
-  // window.setInterval(draw, 1000 / 60)
-
-  window.requestAnimationFrame(draw)
+  eng.setEvents(gameEl, state)
+  draw()
 }
 
 function Engine (ctx) {
@@ -87,22 +89,20 @@ function Engine (ctx) {
     ctx.fillRect(x, y, w, h)
   }
 
-  function drawPlayer (x, y) {
+  function drawPlayer (x, y, cache) {
+    if (cache) {
+      ctx.clearRect.apply(ctx, cache)
+    }
     return ctx.fillRect(x, y, 25, 100)
   }
 
-  function drawNetTile (y) {
-    ctx.fillRect(390, y, 20, 20)
-  }
-
-  function drawTiles () {
-    for (let i = 10; i <= 800; i = i + 40) {
-      drawNetTile(i)
-    }
-  }
-
   function drawNet () {
-    return drawTiles()
+    ctx.save()
+    ctx.fillStyle = '#111'
+    for (let y = 10; y <= 800; y = y + 40) {
+      ctx.fillRect(390, y, 20, 20)
+    }
+    ctx.restore()
   }
 
   let prevBallPosition
@@ -112,10 +112,14 @@ function Engine (ctx) {
       const [dx, dy] = [x1 - x0, y1 - y0]
       ctx.clearRect.apply(ctx, prevBallPosition)
       ctx.save()
-      drawBallBlur(x0, y0, dx, dy)
+      if (dx === 0 && dy === 0) {
+        ctx.fillRect(x1, y1, 15, 15)
+      } else {
+        drawBallBlur(x0, y0, dx, dy)
+      }
+      ctx.restore()
     }
-    ctx.restore()
-    // ctx.fillRect(x1, y1, 15, 15)
+
     prevBallPosition = [x1, y1, 15, 15]
   }
 
@@ -126,7 +130,20 @@ function Engine (ctx) {
     ctx.fillText(score2, 450, 50)
   }
 
+  function handleMousemove (state, top, bottom) {
+    return (event) => {
+      const yPos = event.clientY
+      state.player1y = yPos - top - 50
+    }
+  }
+
+  function setEvents (element, state) {
+    const {top, bottom} = element.getBoundingClientRect()
+    element.onmousemove = handleMousemove(state, top, bottom)
+  }
+
   return {
+    setEvents: setEvents,
     drawScores: drawScores,
     drawPlayer: drawPlayer,
     drawBall: drawBall,
